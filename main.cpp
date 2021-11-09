e0c7b2fa87f70e659d1c3661e06ed762578aac23
MattiaMosso <91486653+MattiaMosso@users.noreply.github.com>
2021-11-09 17:41:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0, i=0, j=1, k=1, l=1;
+    cin >> n;
+    if (n>1){
+        cout << 1 <<endl;
+        for (l=0;l<n-1;l++){
+        k=i+j;
+        cout << k <<endl;
+        i=j;
+        j=k;
+        }
+    }else{
+        cout << "errore" <<endl;
+    }
+    return 0;
 }
